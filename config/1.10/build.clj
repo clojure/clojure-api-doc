a98b8bf3b4566bd3ab168812cfd92d787324254a
Alex Miller <alex.miller@cognitect.com>
2018-12-17 11:35:55 -0600

    1.10 api doc update

--- config/1.10/build.clj
+++ config/1.10/build.clj
@@ -12,12 +12,12 @@
 (def shared (edn-read "../params.clj"))
 
 (p/merge-params
-  (merge shared
+  (merge shared    
     {:root (str (.getAbsolutePath (File. "../../repo")) "/")
      :output-path (str (.getAbsolutePath (File. "../../repo-docs")) "/")}))
 
-(let [branch-info {:name "master" :version "v1.11" :status "in development"}
+(let [branch-info {:name "clojure-1.10.0" :version "v1.10" :status "stable" :first? true}
       all-branch-info (:branches shared)]
 
-  (d/xform-tree "../../repo/doc" "../../repo-docs/branch-master/doc")
+  (d/xform-tree "../../repo/doc" "../../repo-docs/doc")
   (h/make-all-pages branch-info all-branch-info (edn-read "analysis.edn")))
---
+++ config/1.10/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+branch=clojure-1.10.0
+
+# Run autodoc-collect
+(cd ../../repo && git checkout "$branch")
+rm -f analysis.edn
+echo "Analyzing $branch"
+cat collect.clj | clojure -C:collect -
+
+# Run autodoc
+echo "Building $branch"
+cat build.clj | clojure -R:build -
