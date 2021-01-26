b7a5e429201a4d2e3523e578ed70f6ee96c5f5d3
Alex Miller <alex.miller@cognitect.com>
2021-01-26 15:13:07 -0600

    update to newer clj

--- config/1.8/build.sh
+++ config/1.8/build.sh
@@ -8,8 +8,8 @@ branch=clojure-1.8.0
 (cd ../../repo && git checkout "$branch")
 rm -f analysis.edn
 echo "Analyzing $branch"
-cat collect.clj | clojure -C:collect -
+cat collect.clj | clojure -M:collect -
 
 # Run autodoc
 echo "Building $branch"
-cat build.clj | clojure -R:build -
+cat build.clj | clojure -M:build -
