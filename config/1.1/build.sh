14fc4e04644f89c58038d371011e4190f4ab84a1
Alex Miller <alex.miller@cognitect.com>
2022-10-25 13:54:55 -0500

    debug

--- config/1.1/build.sh
+++ config/1.1/build.sh
@@ -8,7 +8,7 @@ branch=1.1.x
 (cd ../../repo && git checkout "$branch")
 rm -f analysis.edn
 echo "Analyzing $branch"
-clojure -Strace -Spath -Sverbose -Sforce -M:collect collect.clj
+clojure -Spath -Sforce -M:collect collect.clj
 
 # Run autodoc
 echo "Building $branch"
