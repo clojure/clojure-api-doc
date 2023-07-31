cd4a95244d0ca9512852423d41ca0fe0d06410e9
JarrodCTaylor <jarrod@fastmail.com>
2023-07-31 16:22:28 -0500

    Update build.sh to be compatible with github action used to build the docs

--- build.sh
+++ build.sh
@@ -2,24 +2,8 @@
 
 set -e
 
-# Create or refresh repo
-if [[ ! -d repo ]]; then
-  echo "Creating new clojure repo area"
-  git clone git@github.com:clojure/clojure.git repo
-else
-  echo "Refreshing clojure repo area"
-  (cd repo && git pull && git reset --hard)
-fi
+cd clojure-api-doc
 
-# Create or clean output directory
-if [[ ! -d repo-docs ]]; then
-  echo "Creating new clojure gh-pages area"
-  git clone git@github.com:clojure/clojure.git repo-docs
-  (cd repo-docs && git checkout gh-pages)
-else
-  echo "Refreshing clojure gh-pages area"
-  (cd repo-docs && git fetch && git clean -d -f && git pull)
-fi
 rm -rf repo-docs/*
 
 # Copy stable site files
@@ -29,18 +13,7 @@ cp -R site/* repo-docs
 # Build all branches
 for branch in config/*/
 do
-  echo "Building $branch"
   cd "$branch"
   ./build.sh
   cd ../..
-done	
-
-# Commit
-if [[ ! -z "$COMMIT" ]]; then
-  echo "Committing updated gh-pages branch"
-  cd repo-docs
-  git add -u -v
-  git commit -m "Autodoc commit"
-  git config --global push.default simple
-  git push
-fi
+done
